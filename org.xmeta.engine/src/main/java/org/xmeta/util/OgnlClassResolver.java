1c8e6e84d88a49550f7ae536e0577f3a07c3b562
Administrator <Administrator@zhangyuxiang>
2013-10-23 09:57:40 +0800

    修改了开原协议，采用GPL

--- org.xmeta.engine/src/main/java/org/xmeta/util/OgnlClassResolver.java
+++ org.xmeta.engine/src/main/java/org/xmeta/util/OgnlClassResolver.java
@@ -1,3 +1,24 @@
+/*
+    X-Meta Engine。
+    Copyright (C) 2013  zhangyuxiang
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    For alternative license options, contact the copyright holder.
+
+    Emil zhangyuxiang@tom.com
+ */
 package org.xmeta.util;
 
 import java.util.Map;
