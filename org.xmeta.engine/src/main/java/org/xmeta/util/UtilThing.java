57c8d1e9b9cbb211bead3207ae9f6b415678d323
zyx <zyx@zhangyuxiang>
2015-12-25 18:54:30 +0800

    定期提交

--- org.xmeta.engine/src/main/java/org/xmeta/util/UtilThing.java
+++ org.xmeta.engine/src/main/java/org/xmeta/util/UtilThing.java
@@ -48,6 +48,29 @@ public class UtilThing {
 	}
 	
 	/**
+	 * 通过事物属性获取事物定义的事物，或者通过指定的子事物的第一个子节点获取定义的事物。
+	 * 
+	 * @param thing
+	 * @param attributeName
+	 * @param childThingPath
+	 * @return
+	 */
+	public static Thing getThingFromAttributeOrChild(Thing thing, String attributeName, String childThingPath){
+		Thing t = null;
+		
+		String attrValue = thing.getString(attributeName);
+		if(attrValue != null && !"".equals(attrValue)){
+			t = World.getInstance().getThing(attrValue);
+		}
+		
+		if(t == null){
+			return thing.getThing(childThingPath);
+		}
+		
+		return t;
+	}
+	
+	/**
 	 * 判断一个事物作为描述者时是否是给定的类型，即判断这个事物的路径以及所有继承事物的路径是否是指定的descritpor。
 	 * 
 	 * @param descriptorThing
